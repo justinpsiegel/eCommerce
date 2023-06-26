fc0b53d5cf7e32c48a74cc4be7c81c4dbf630ec9
justinpsiegel <justinpsiegel@gmail.com>
2023-06-26 18:05:27 -0400

    Added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css";
+import Logo from "../assets/Gull.png";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-[lightblue]">
-        <div className="links">
-          <Link
-            to="/"
-            className="hover:bg-blue-500 rounded w-[80px] h-[40px] flex justify-center transition-all items-center"
-          >
-            Shop
+      <div className="flex">
+        <div>
+          <Link to="/">
+            <img
+              className="m-8"
+              src={Logo}
+              alt="Logo"
+              style={{ width: "75px" }}
+            />
           </Link>
-          <Link
-            to="/cart"
-            className="hover:bg-blue-500 rounded w-[80px] h-[40px] flex justify-center transition-all items-center"
-          >
-            <ShoppingCart size="32" />
-          </Link>
+        </div>
+        <div className="navbar bg-[lightblue]">
+          <div className="links">
+            <Link
+              to="/"
+              className="hover:bg-blue-500 rounded w-[80px] h-[40px] flex justify-center transition-all items-center"
+            >
+              Shop
+            </Link>
+            <Link
+              to="/cart"
+              className="hover:bg-blue-500 rounded w-[80px] h-[40px] flex justify-center transition-all items-center"
+            >
+              <ShoppingCart size="32" />
+            </Link>
+          </div>
         </div>
       </div>
       <h1 className="flex justify-center my-4 text-5xl">Shop of The Gull</h1>
