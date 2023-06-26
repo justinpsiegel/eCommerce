7b9c357c4690dfff85d303496da43306ef423574
justinpsiegel <justinpsiegel@gmail.com>
2023-06-26 13:29:24 -0400

    Shop is mostly functionally complete

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
+import { fetchProducts } from "../products";
 
 export const ShopContext = createContext(null);
 
@@ -13,9 +14,38 @@ const getDefaultCart = () => {
 const localStorageCart = JSON.parse(localStorage.getItem("cart"));
 
 const ShopContextProvider = (props) => {
+  const [data, setData] = useState(null);
+  const [getTotalCartAmount, setGetTotalCartAmount] = useState(0);
   const [cartItems, setCartItems] = useState(
     localStorageCart ? localStorageCart : getDefaultCart()
   );
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      try {
+        const fetchedData = await fetchProducts();
+        setData(fetchedData);
+      } catch (error) {}
+    };
+
+    fetchDataAsync();
+
+    const getTotalCartAmount = async () => {
+      const fetchedData = await fetchProducts();
+      let totalAmount = 0;
+      for (const item in cartItems) {
+        if ((await cartItems[item]) > 0) {
+          let itemInfo = fetchedData?.find(
+            (product) => product.id === Number(item)
+          );
+          console.log(itemInfo);
+          totalAmount += cartItems[item] * itemInfo.price;
+        }
+      }
+      setGetTotalCartAmount(totalAmount);
+    };
+    getTotalCartAmount();
+  }, []);
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
@@ -34,6 +64,7 @@ const ShopContextProvider = (props) => {
     addToCart,
     removeFromCart,
     updateCartItemCount,
+    getTotalCartAmount,
   };
 
   return (
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -5,9 +5,11 @@ import { ShopContext } from "../../context/ShopContext";
 import CartItem from "./CartItem";
 import "./cart.css";
 
+import { useNavigate } from "react-router-dom";
+
 const Cart = () => {
   const [data, setData] = useState(null);
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
 
   useEffect(() => {
     const fetchDataAsync = async () => {
@@ -20,18 +22,40 @@ const Cart = () => {
     fetchDataAsync();
   }, []);
 
+  const navigate = useNavigate();
+
   return (
     <div className="cart">
       <div>
         <h1 className="text-5xl my-8">Cart</h1>
       </div>
-      <div className="cartItems">
-        {data?.map((product) => (
-          <div>
-            {cartItems[product.id] !== 0 ? <CartItem data={product} /> : <></>}
+      {data ? (
+        <div className="cart">
+          <div className="cartItems">
+            {data?.map((product) => (
+              <div>
+                {cartItems[product.id] !== 0 ? (
+                  <CartItem data={product} />
+                ) : (
+                  <></>
+                )}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+          {getTotalCartAmount > 0 ? (
+            <div className="checkout">
+              <p>Subtotal: ${getTotalCartAmount}</p>
+              <button onClick={() => navigate("/")}>Continue Shopping</button>
+              <button>Checkout</button>
+            </div>
+          ) : (
+            <div>Your Cart is empty</div>
+          )}
+        </div>
+      ) : (
+        <div>Fetching Your Cart...</div>
+      )}
+      {localStorage.setItem("cart", JSON.stringify(cartItems))}
     </div>
   );
 };
